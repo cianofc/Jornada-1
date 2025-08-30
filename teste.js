5fde1030cd0fb40e4687a8ea30791d3fb648b756
cianofc <cauashioficial@gmail.com>
2025-08-30 19:00:10 -0300

    update teste.js

--- teste.js
+++ teste.js
@@ -1,1 +1,26 @@
 console.log('Olá amigo');
+
+let player = "Rodrigo Faro";
+let vida = 8;
+
+// resumo do código acima:
+
+//tipo de variável:
+
+//let é responsável por mudar o valor da variável ou seja ela é o tipo da variável! Existem 2 principais variáveis: "const" responsável por aplicar um valor não alteravel/valor não muda só é aplicado e fica permanente; e "let" q muda o valor e vc pode alterar o valor a vontade usando outras linhas de código. 
+
+// variável:
+
+// variável é o nome q vc dar depois de let.
+
+// valor da variável:
+
+//pode ser um nome ou número 
+//= "Rodrigo Faro"
+//= "8"
+
+console.log(player);
+console.log(vida);
+
+// consolo.log("nome da variável") 
+// é responsável por exibir no console um texto.
